919fdc1141da00a12067e2e1a4058ff9b41c18a1
Azamat Murzaliev <lirog.kg@gmail.com>
2022-11-15 18:16:25 +0600

    Переделал события закрытия алертов

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -7,15 +7,13 @@ interface AlertProps extends React.PropsWithChildren{
 }
 
 const Alert: React.FC<AlertProps> = ({show,type, onDismiss, children}) => {
-  const alertClass = "alert-dismissible alert alert-" + type;
+  const alertClass = "w-50 mt-3 alert-dismissible alert alert-" + type;
 
   return (
       <div className={alertClass} style={{display: show ? 'block' : 'none'}} role="alert">
         {children}
-        {/*{alertCancel}*/}
         {onDismiss ? (<button onClick={onDismiss} type="button" className="btn-close"></button>) : null}
       </div>
   );
 };
-// style={{display: show ? 'block' : 'none'}}
 export default Alert;
