d21a4d0fa75074df6a3e328a080110af7b2e644b
Azamat Murzaliev <lirog.kg@gmail.com>
2022-11-15 09:25:26 +0600

    Выполнил 2 основных задания
    Создал реакт приложение, удалил ненужные файлы, создал 3 компонента Modal, Backdrop, Alert. Прописал структуру, состояния и функции для их реализации.

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -9,7 +9,6 @@ interface AlertProps extends React.PropsWithChildren{
 const Alert: React.FC<AlertProps> = ({show,type, onDismiss, children}) => {
   const alertClass = "alert-dismissible alert alert-" + type;
 
-
   return (
       <div className={alertClass} style={{display: show ? 'block' : 'none'}} role="alert">
         {children}
